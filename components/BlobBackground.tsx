e5c4be461ea16dcf38c7d611a36d5edafabe8e76
nlwong168 <nlwong168@gmail.com>
2026-04-14 14:52:57 -0500

    fix: replace SVG goo filter with CSS blur for iOS compat, reduce opacity, center blobs vertically

--- components/BlobBackground.tsx
+++ components/BlobBackground.tsx
@@ -8,19 +8,6 @@ export default function BlobBackground() {
   const { circles } = useBlobConfig()
   return (
     <div className={styles.root} aria-hidden="true">
-      <svg className={styles.filter}>
-        <defs>
-          <filter id="blob-goo">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="20" result="blur" />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 26 -10"
-              result="goo"
-            />
-          </filter>
-        </defs>
-      </svg>
       <div className={styles.container}>
         {circles.map((c, i) => (
           <div
